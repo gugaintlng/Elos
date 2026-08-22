4f9d90bf67bd5399629554533e5f89e5f263e878
gugaintlng <guga44grijo@gmail.com>
2026-08-22 21:12:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    um utente, uma falha de wi-fi não pode interromper nada.
    ============================================================= */
 
-const VERSAO = 'elos-v1.9.1';
+const VERSAO = 'elos-v1.9.2';
 
 const ESSENCIAIS = [
   './',
